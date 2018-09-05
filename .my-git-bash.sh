04809412f31c240a7280349a182d52c1fa2d0930
Jeff Doolittle <jeff.doolittle@lotpath.com>
2018-09-05 08:17:33 -0700

    initial commit

---
+++ .my-git-bash.sh
@@ -0,0 +1,10 @@
+source ~/.git-completion.bash
+source ~/.git-prompt.sh
+GIT_PS1_SHOWDIRTYSTATE=true
+GIT_PS1_SHOWUPSTREAM=verbose
+GIT_PS1_SHOWCOLORHINTS=true
+GIT_PS1_SHOWUNTRACKEDFILES=true
+PROMPT_COMMAND='__git_ps1 "\w" " \$ "'
+
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/git/bin:/usr/local/sbin
+
